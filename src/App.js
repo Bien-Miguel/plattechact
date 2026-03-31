a8bbcfb889d9fde217c15b7eee6739ec04d1de35
Bien-Miguel <lamanobienmiguel@gmail.com>
2026-03-31 21:20:51 +0800

    Added App component and index.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
 import { db } from './firebase';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Student Record System</h1>
+      <p>If you see this, your Vercel deployment is WORKING!</p>
+      {/* Your form code from the lab manual goes here later */}
+    </div>
+  );
+}
+
+export default App;
